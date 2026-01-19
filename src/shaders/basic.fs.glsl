a55e21071dfe66f62e83f9ed224def3fff44db19
AlexisCroteau034 <alexis.croteau@polymtl.ca>
2026-01-19 15:17:53 -0500

    Basic shaders

--- src/shaders/basic.fs.glsl
+++ src/shaders/basic.fs.glsl
@@ -2,8 +2,11 @@
 
 // TODO: La couleur des fragments est donnée à partir de la couleur
 //       des vertices passée en entrée.
+in vec3 color;
+
+out vec3 fragColor;
 
 void main()
 {
-    
+    fragColor = color;
 }
